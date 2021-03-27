3256ccbf69d187feb786c3086d0c9204f56fc0e1
Ahmad Allehyany <aallehyany@gmail.com>
2021-03-27 22:22:43 +0300

    added container and layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,70 @@
+import React from 'react';
 import styled from 'styled-components'
+import {Reset} from 'styled-reset';
+import {ThemeProvider} from 'styled-components';
+import * as theme from '../theme/theme';
+import PageHeader from '../components/Header';
+import SearchBar from '../components/SearchBar';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+import Note from '../components/Note';
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  min-width: 100%;
+  font-family: sans-serif;
+  box-sizing: border-box;
+  flex-flow: column nowrap;
+  @media(min-width: 992px) {
+    min-width: 960px;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  @media(min-width: 992px) {
+    flex-direction: row;
+    align-items: start;
+
+  }
+
+`;
+
+const NotesContainer = styled.div`
+  display: flex;
+  padding: 0.25rem;
+  width: 100%;
+  box-sizing: border-box;
+  flex-flow: column nowrap;
+`;
+
+const MainContent = styled.div`
+  flex: 1;
+  padding: 0.2rem;
+  border-left: 1px solid #eeeeee;
+`;
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <ThemeProvider theme={theme}>
+    <Container>
+      <Reset />
+      <PageHeader />
+      <Content>
+        <MainContent>
+          <SearchBar />
+          <NotesContainer>
+            <Note />
+            <Note />
+          </NotesContainer>
+        </MainContent>
+      </Content>
+    </Container>
+  </ThemeProvider>
 }
